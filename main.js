a9324e956135b48ceb8a329a1ddc47fa61ae8c36
fxyf1991 <fxyf1991@gmail.com>
2018-01-18 14:56:11 +0800

    浮层开关演示

---
+++ main.js
@@ -0,0 +1,9 @@
+$(clickMe).on('click', function() {
+  $(popover).toggle()
+  $(document).one('click', function() {
+    $(popover).toggle()
+  })
+})
+$(wrapper).on('click', function(e){
+  e.stopPropagation()
+})
